dd9bb6e557b1d8f0d2f7e5d1243d2e92ed2d75bb
ruidantas <ruifisicauesb@hotmail.com>
2022-09-01 23:02:22 -0300

    update2

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
 const productController = require("../controllers/products.controllers");
-// const productsMiddleware =
+const BodyMiddleware = require("../middlewares/bodyMiddleware");
+const idMiddleware = require("../middlewares/idMIddleware");
+const putMiddleware = require("../middlewares/putMiddleware")
 
 router.get("/", productController.getAllProductsController);
-router.get("/getById/:id", productController.getProductByIdController);
-router.delete("/delete", productController.deleteProductByIdController);
-router.post("/post", productController.postProductController);
-router.put("/put", productController.putProductController);
+router.get("/getById/:id", idMiddleware, productController.getProductByIdController);
+router.delete("/delete/:id", idMiddleware, productController.deleteProductByIdController);
+router.post("/post", BodyMiddleware, productController.postProductController);
+router.put("/put/:id", putMiddleware, productController.putProductController);
 
-module.exports = router
+module.exports = router;
